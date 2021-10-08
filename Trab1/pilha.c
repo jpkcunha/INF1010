6d93f2e91ec6663976a6d65080a6c62340728b77
Joao Pedro Cunha <jpkcunha@gmail.com>
2021-10-08 19:25:27 -0300

    TAD - Pilha Genérica

--- Trab1/pilha.c
+++ Trab1/pilha.c
@@ -2,83 +2,56 @@
 #include <stdlib.h>
 #include "pilha.h"
 
-typedef struct elemento {
+typedef struct elemento
+{
     void* info;
-    int tipo, prioridade;
     struct elemento* prox;
 } Elemento;
 
 
-struct pilha{
+struct pilha
+{
     Elemento *topo;
 };
 
-void aborta(char* s){
+void aborta(char* s)
+{
     printf("%s",s);
     exit(1);
 }
 
 
-Pilha* pilha_cria(void){
+Pilha* pilha_cria(void)
+{
     Pilha* p = (Pilha*)malloc(sizeof(Pilha));
     if (p == NULL) aborta("Erro de alocacao dinamica da pilha\n");
     p->topo = NULL;
     return p;
 }
 
-int pilha_vazia(Pilha* p){
+int pilha_vazia(Pilha* p)
+{
     return (p->topo == NULL);
 }
 
 
-void pilha_push(Pilha* p, int tipo, void* x){
+void pilha_push(Pilha* p, void* info)
+{
     Elemento* novo = (Elemento*)malloc(sizeof(Elemento));
     if (novo == NULL) aborta("Erro de alocacao dinamica do elemento\n");
-
-    novo->prioridade = INT;
-    novo->tipo = tipo;
-    if (tipo){
-        char c = *((char*)x);
-        switch (c){
-            case '+':
-                novo->prioridade = AD_SUB;
-                break;
-            case '-':
-                novo->prioridade = AD_SUB;
-                break;
-            case '*':
-                novo->prioridade = MULT_DIV;
-                break;
-            case '/':
-                novo->prioridade = MULT_DIV;
-                break;
-            case '(':
-                break;
-            case ')':
-                break;
-            default:
-                printf("%c - ",c);
-                aborta("Símbolo invalido\n");
-        }
-    }
-    novo->info = x;
+    novo->info = info;
     novo->prox = p->topo;
     p->topo = novo;
 }
 
-void* pilha_pop(Pilha* p, int* tipo, int *prioridade)
+void* pilha_pop(Pilha* p)
 {
     Elemento* t;
     void* val;
     if (pilha_vazia(p)) aborta("Pilha vazia\n");
-
     t = p->topo;
-    *tipo = t->tipo;
-    *prioridade = t->prioridade;
     val = t->info;
     p->topo = t->prox;
-
-    //free(t->info);
     free(t);
     return val;    
 }
--- Trab1/pilha.h
+++ Trab1/pilha.h
@@ -1,22 +1,13 @@
 typedef struct pilha Pilha;
 
-static const int PAR_ESQ = -3;
-static const int PAR_DIR = -2;
-static const int OP = -1;
-static const int INT = 0;
-static const int AD_SUB = 1;
-static const int MULT_DIV = 2;
-
 void aborta(char* s);
 
 Pilha* pilha_cria(void);
 
 int pilha_vazia(Pilha* p);
 
-void pilha_push(Pilha* p, int tipo, void* x);
+void pilha_push(Pilha* p, void* info);
 
-void* pilha_pop(Pilha* p, int* tipo, int *prioridade);
-
-//char pilha_pop_op(Pilha* p);
+void* pilha_pop(Pilha* p);
 
 void pilha_libera(Pilha* p);
